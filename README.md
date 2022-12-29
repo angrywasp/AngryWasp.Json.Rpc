e6ade0e8f466ee37391f1ae7c2fbf8f043cbb218
angrywasp <angrywasp@live.com>
2022-12-29 22:38:40 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AngryWasp.Json.Rpc
---
+++ Src/AngryWasp.Json.Rpc.Test/Src/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AngryWasp.Json.Rpc.Test
+{
+    internal class MainClass
+    {
+        private static void Main(string[] rawArgs)
+        {
+
+        }
+    }
+}
